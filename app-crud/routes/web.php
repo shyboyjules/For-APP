928c82224490027a94433f6ff7177462bb8a6d54
shyboyjules <161744986+shyboyjules@users.noreply.github.com>
2024-06-17 21:28:02 +0800

    Update web.php
    removed a line

--- app-crud/routes/web.php
+++ app-crud/routes/web.php
@@ -26,7 +26,6 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::post("/uploadfood", [ProductController::class, 'upload']);
-    Route::get("/deletemenu/{id}", [ProfileController::class, 'deletemenu']);
     
 });
 
